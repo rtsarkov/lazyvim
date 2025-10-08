f9e41cebf2d50165b8dd6ea91bda8095e657e5f3
rtsarkov <tsarkofff1500@yandex.ru>
2025-10-08 21:56:40 +0300

    save

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,13 @@
 return {
   -- добавляем плагин с темой
-  --{ "catppuccin/nvim" },
-  { "everviolet/nvim" },
-  -- настраиваем LazyVim, чтобы использовать эту тему по умолчанию
+  { "catppuccin/nvim" },
+  --{ "everviolet/nvim" },
+  --gruvbox-material настраиваем LazyVim, чтобы использовать эту тему по умолчанию
   {
     "LazyVim/LazyVim",
     opts = {
-      --colorscheme = "gruvbox-material",
-      --colorscheme = "catppuccin",
-      colorscheme = "evergarden",
+      colorscheme = "catppuccin",
+      --colorscheme = "evergarden",
     },
   },
 }
